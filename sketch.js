4831ea15168a70350beb9fbb6ec61f02761fd663
A4R4V-FF <89017796+A4R4V-FF@users.noreply.github.com>
2021-10-12 21:15:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   angleMode(DEGREES);
-
+  var options ={ isStatic:true }
   //create player base body
   playerBase = Bodies.rectangle(200,350,180,150,options);
   World.add(world,playerBase);
